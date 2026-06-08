394fd550608ac68a4c70b18a831ae96beddd057c
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-08 19:36:06 +0530

    added empty array a case and print SOLN

--- Remove_elements(07-06-2026).py
+++ Remove_elements(07-06-2026).py
@@ -1,10 +1,13 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
+        if nums is None:
+            return 0
         write_index = 0
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[write_index] = nums[i]
                 write_index += 1
         return write_index
-nums = [3, 2, 2, 3]
+nums = [3, 3, 3, 3]
 val = 3
+print(Solution().removeElement(nums, val))  # Output: 2
