e4aaad0e73a31a979bc8324f66db39a0647527ea
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-14 19:23:22 +0530

    commit
    Duplicate_sort(060-06-2`#

--- Find first occurence in string(08-06-2026).py
+++ Find first occurence in string(08-06-2026).py
@@ -1,9 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
         for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle:
+            if haystack[i: i + len(needle)] == needle:
                 return i
         return -1
-    haystack = "hello"
-    needle = "ll"   
-    print(Solution().strStr(haystack, needle))  # Output: 2
+haystack = "heeeeeello"
+needle = "ll"   
+print(Solution().strStr(haystack, needle))  # Output: 2
