133b21822692cf9173d8988f3eeaa900c7c53a6b
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-16 15:50:49 +0530

    Initial Code

---
+++ Valid Palindrome(16-06-2026).py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pal =[]
+        newstr  = s.lower()
+        for char in newstr:
+            if char.isalnum():
+                pal.append(char)
+        cleaned = "".join(pal)
+
+        left =0
+        right = len(cleaned)-1
+        while left <= right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
