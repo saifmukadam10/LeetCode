0e01e8d35f54ef059fe0eb72c1bd2de3e970968a
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-17 15:57:18 +0530

    Time complexity

--- Three sum(17-06-2026).py
+++ Three sum(17-06-2026).py
@@ -13,7 +13,7 @@ class Solution:
             while left< right:
                 total = nums[i] + nums[left] + nums[right]
                 if total == 0:
-                    res.append(nums[i], nums[left], nums[right])
+                    res.append([nums[i], nums[left], nums[right]])
 
                     while left < right and nums[left]== nums[left+1]:
                         left +=1
@@ -29,6 +29,9 @@ class Solution:
                 else:
                     left +=1
         return res
+    
+nums = [-1, 0, 1, 2, -1, -4]
+print(Solution().threeSum(nums))
 
 
 
