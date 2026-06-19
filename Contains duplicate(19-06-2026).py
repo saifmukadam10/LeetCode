74f65eb4c2459406ecb36d8c202ad6bb84b65399
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-19 20:00:11 +0530

    Hash Map

--- Contains duplicate(19-06-2026).py
+++ Contains duplicate(19-06-2026).py
@@ -17,7 +17,15 @@ class Solution:
             seen.add(num)
         return False
 
-    
+    def containsDuplicate3(self, nums: list[int]) -> bool:
+        #HASH MAP APPROACH
+        seen ={}
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
+
 nums = [1, 2, 3, 1]
-print(Solution().containsDuplicate2(nums))
+print(Solution().containsDuplicate3(nums))
 # time complexity: O(n log n) due to sorting
