3bd9f1c94a9e9e14634d8c5977d29234f567839d
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-22 22:03:03 +0530

    Best time to buy 2 stocks

---
+++ best time to buy or sell stock 3 (22-06-2026).py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        if not prices:
+            return 0
+        
+        first_buy = float('inf')
+        first_profit = 0
+        second_buy = float('inf')
+        second_profit = 0
+
+        for i in range(len(prices)):
+            first_buy = min(first_buy, prices[i])
+            first_profit = max(first_profit, prices[i] - first_buy)
+            second_buy = min(second_buy, prices[i] - first_profit)
+            second_profit = max(second_profit, prices[i] - second_buy)
+        return second_profit
