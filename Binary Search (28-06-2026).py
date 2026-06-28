bd1d9d02896621e5233f8e3f7b9056d3017420f1
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-28 17:39:12 +0530

    Test Case & Print Solution

--- Binary Search (28-06-2026).py
+++ Binary Search (28-06-2026).py
@@ -12,3 +12,6 @@ class Solution:
             else:
                 right = middle - 1
         return -1
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 5
+print(Solution().binarySearch(nums, target))
