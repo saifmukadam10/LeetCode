fe6f8be63f8771378cef0688cb8c10e4d0120aa8
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-29 23:11:23 +0530

    Running Sum arr

--- Running Sum of 1D array(29-06-2026).py
+++ Running Sum of 1D array(29-06-2026).py
@@ -1,10 +1,13 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum(self, nums: list[int]) -> list[int]:
         sum = 0
         arr =[]
         for i in range (len(nums)):
             sum += nums[i]
             arr.append(sum)
         return arr
+nums = [1, 2, 3, 4]
+print(Solution().runningSum(nums))
+#time complexity is O(n) and space complexity is O(n)
         
         
