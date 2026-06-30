ad8467493defc3d8150a4942e09ed142488bbfce
Saif Mukadam <mukadamsaif9@gmail.com>
2026-06-30 23:18:21 +0530

    Prefix Sum

---
+++ Find pivot index(30-06-3036).py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+        
+        for i, num in enumerate(nums):
+            # Right sum is everything else minus left_sum and the current number
+            if left_sum == (total_sum - left_sum - num):
+                return i
+            
+            # Update left_sum for the next iteration
+            left_sum += num
+            
+        return -1
