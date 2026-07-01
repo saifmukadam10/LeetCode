fdf49535c826b7df56543509df0ba07308c2ef09
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-01 21:08:15 +0530

    solution

--- Maximum Subarray (1-07-2026).py
+++ Maximum Subarray (1-07-2026).py
@@ -4,6 +4,8 @@ class Solution:
         current_sum = nums[0]
 
         for i in range(1, len(nums)):
-            currrent_sum = max(nums[i], current_sum + nums[i])
+            current_sum = max(nums[i], current_sum + nums[i])
             max_sum = max(max_sum, current_sum)
         return max_sum
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(Solution().maxSubArray(nums))
