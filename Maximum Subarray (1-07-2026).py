20ae8eb2651805f2c61187785ccc98269cbc786d
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-01 21:09:43 +0530

    Kadane's Algo

--- Maximum Subarray (1-07-2026).py
+++ Maximum Subarray (1-07-2026).py
@@ -2,10 +2,12 @@ class Solution:
     def maxSubArray(self, nums : list[int]) -> int:
         max_sum = nums[0]
         current_sum = nums[0]
+        n = len(nums)
 
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             current_sum = max(nums[i], current_sum + nums[i])
             max_sum = max(max_sum, current_sum)
         return max_sum
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 print(Solution().maxSubArray(nums))
+#time complexity is O(n) and space complexity is O(1)
