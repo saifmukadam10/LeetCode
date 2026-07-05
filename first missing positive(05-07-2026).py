1d6e23b618e4579c28782f2963d8f1b6e63446c7
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-05 20:30:39 +0530

    HARD

--- first missing positive(05-07-2026).py
+++ first missing positive(05-07-2026).py
@@ -1,17 +1,16 @@
 class Solution:
     def firstMissingPositive(self, nums: list[int]) -> int:
-
-        for i in range(len(nums)):
-            while 1 <= nums[i] <= len(nums) and nums[nums[i]-1] != nums[i]:
-
-                correct_idx = nums[i] -1
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i]-1] != nums[i]:
+                correct_idx = nums[i] - 1
                 nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
-        for i in range(len(nums)):
+        for i in range(n):
             if nums[i] != i + 1:
                 return i + 1
         
         # Step 3: If all numbers 1 to n are present, return n + 1
-        return len(nums) + 1
+        return n + 1
 nums = [3, 4, -1, 1]
 print(Solution().firstMissingPositive(nums))
         
