76e068c705b850e77c3ba22f6a6daef1c11efb39
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-05 20:28:40 +0530

    Initial

---
+++ first missing positive(05-07-2026).py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+
+        for i in range(len(nums)):
+            while 1 <= nums[i] <= len(nums) and nums[nums[i]-1] != nums[i]:
+
+                correct_idx = nums[i] -1
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        # Step 3: If all numbers 1 to n are present, return n + 1
+        return len(nums) + 1
+        
