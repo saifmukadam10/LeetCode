ca25acfbb6df88563f6ad19d8ed86e5258782e75
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-07 23:03:01 +0530

    initial

---
+++ valid anagram(07-07-20260.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # If lengths are different, they cannot be anagrams
+        if len(s) != len(t):
+            return False
+        
+        count_s = {}
+        count_t = {}
+        
+        # Build frequency maps for both strings
+        for i in range(len(s)):
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
+            
+        # Compare the two dictionaries
+        return count_s == count_t
