bb7bbbc887249952a52dfa0271a3ddfd3b55704c
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-11 23:28:15 +0530

    Initial

---
+++ First Unique Char in String(11-07-2026).py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+        
+        # Count the occurrences of each character
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+        
+        # Find the index of the first unique character
+        for i, char in enumerate(s):
+            if freq[char] == 1:
+                return i
+        
+        return -1
