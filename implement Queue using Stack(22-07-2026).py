9c8e5ed5340a2ea03275a876f2f930c88d720bdf
Saif Mukadam <mukadamsaif9@gmail.com>
2026-07-22 23:36:21 +0530

    STACK

--- implement Queue using Stack(22-07-2026).py
+++ implement Queue using Stack(22-07-2026).py
@@ -33,4 +33,8 @@ class MyQueue:
 
 
 # Your MyQueue object will be instantiated and called as such:
- 
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
